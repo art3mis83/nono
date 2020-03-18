83eb4d6cb51bae99bca9bea2a803ac59e9c59f8c
Your Name <you@example.com>
2020-03-18 21:02:03 +0100

    Ajout des tags (gre, ocean, stef) + wesh

--- index.js
+++ index.js
@@ -22,6 +22,36 @@ bot.on('ready', function () {
         if (myMessage.indexOf('BONSOIR') == 0  ) {
             message.channel.send('Bonsoir ' + message.author.username)  
         }
+
+        if (myMessage.indexOf('COUCOU') == 0  ) {
+          message.channel.send('Qui que tu sois, coucou à toi aussi !')  
+        }
+
+        if (myMessage.indexOf('GRE') != -1 || myMessage.indexOf('GRENOUILLE') != -1  ) {
+          if(message.channel.name != "guerre--binomes"){
+            message.channel.send('!pokemon Froakie')  
+          }
+        }
+
+        if (myMessage.indexOf('OCEAN') != -1 ) {
+          if(message.channel.name != "guerre--binomes"){
+            message.channel.send(':fries: :man_student:')  
+          }
+        }
+
+        if (myMessage.indexOf('STEF') != -1 ) {
+          if(message.channel.name != "guerre--binomes"){
+            message.channel.send(':baby:')  
+          }
+        }
+
+        if (myMessage.indexOf('WESH') >= 0  ) {
+          var responseArray = ['Wesh gros : https://www.youtube.com/watch?v=X6MxGJ7qxck',
+                              'Utilise encore une seule fois ce terme et je mets en marche le trébuchet dans la cour pour t\'envoyer la dernière edition du Larousse en pleine poire',
+                              'Bien ou quoi fraté ?'];
+
+            message.channel.send(responseArray[getRandomInt(2)]); 
+        }
     
         if (myMessage.indexOf('ÇA VA ?') == 0  ) {
             var responseArray = ['Je suis connecté',
@@ -30,12 +60,20 @@ bot.on('ready', function () {
                                     "Très bien, par les temps qui courent, j'attends juste le soulèvement des machines #Terminator",
                                     "Tranquillou ! Tu as bien dormi ?",
                                     "C'est à cette heure ci que tu arrives ? La guerre va pas se gagner toute seule.... sauf si on est pas en jour de guerre"];            
-            
-            
-            
-
+                     
             var index = getRandomInt(5);
             message.channel.send(responseArray[index]);
+        }
+
+        if(myMessage == 'lexique'){
+          message.channel.send("Voilà les leçons que je connais pour le moment : \
+          - bonjour (car il faut être poli)\
+          - coucou (piou piou)\
+          - bonsoir (car il faut être poli le soir aussi)\
+          - ca va ? (le ? est important, sinon je ne peux pas deviner que c'est une question)\
+          - wesh (#Ocean)\
+          - ping (apporte ta raquette)");
+
         }
     }  
     
